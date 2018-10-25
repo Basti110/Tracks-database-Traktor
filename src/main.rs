860642ac1c053c56fafa16681c60fbdd2fc598d1
Basti110 <basti.preuss95@gmail.com>
2018-10-25 19:50:42 +0200

    added get_author_name() and new string traits

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::path::Path;
 const GENERATE_DATA: bool = false;
 const TRACK_LIST_PATH: &str = "src/files/tracks-sample.txt";
 const FILE_DIR: &str = "files/";
+const MAX_FILE_NAME_LEN: usize = 84;
 
 fn main() -> io::Result<()> {
     if GENERATE_DATA {
@@ -21,13 +22,8 @@ fn write_files_from_list() -> io::Result<()> {
     let f = File::open(TRACK_LIST_PATH)?;
     let reader = BufReader::new(f);
     println!("{}", "Generate files");
-    //let mut buffer = String::new();
     let begin = String::from("files/");
-    // read a line into buffer
     for buffer in reader.lines() {
-        //reader.read_line(&mut buffer)?;
-        //println!("{}", buffer);
-        //let mut file = File::create(&buffer)?;
         let line = buffer.unwrap().clone();//buffer.clone();
         let mut dirs: Vec<&str> = line.split("/").collect();
         let mut path = begin.clone();
@@ -40,31 +36,34 @@ fn write_files_from_list() -> io::Result<()> {
             path.push('/');
         }
         path.push_str(dirs[dirs.len() - 1].clone());
-        //path.pop();
         path = path.replace("\"", "");
         path = path.replace("?", "");
-        //println!("{}", path);
         if !Path::new(&path).exists() {
             let mut file = File::create(&path)?;
             file.write_all(b"Test File!")?;
         }
-        //buffer = String::from("");;
     }
     Ok(())
 }
 
 fn check_files(path: &str) -> io::Result<()> {
     let folders = fs::read_dir(path)?;
-    
+    let mut count = 0;
     for folder in folders {
         let folder_path: String = folder.unwrap().path().display().to_string();
         //println!("Name: {}", path);
         let files = fs::read_dir(folder_path)?;
         for file in files {
             let file_name = get_file_name(file)?;
-            println!("Name: {}", file_name);
+            if file_name.len() > MAX_FILE_NAME_LEN {
+                let author = get_author_name(&file_name)?;
+                println!("Name: {}", author);
+                println!("Name: {}", file_name);
+                count += 1;
+            }
         }
     }
+    println!("Count {}; ", count);
     Ok(())
 }
 
@@ -83,4 +82,28 @@ fn get_file_name(file: Result<DirEntry, Error>) -> io::Result<String> {
         None => return Err(Error::new(ErrorKind::InvalidData, "Can not convert path to UTF-8 string")),
     };
     Ok(file_name.to_string())
+}
+
+fn get_author_name(file_name: &String) -> io::Result<String> {
+    let pos = file_name.get_pos('-')?;
+    let author = file_name.substring(0, pos);
+    Ok(author)
+}
+
+trait StringUtils {
+    fn substring(&self, start: usize, len: usize) -> Self;
+    fn get_pos(&self, character: char) -> io::Result<usize>;
+}
+
+impl StringUtils for String {
+    fn substring(&self, start: usize, len: usize) -> Self {
+        self.chars().skip(start).take(len).collect()
+    }
+
+    fn get_pos(&self, character: char) -> io::Result<usize> {
+        match self.chars().position(|c| c == character) {
+            Some(x) => Ok(x),
+            None => Err(Error::new(ErrorKind::InvalidData, "Can not find char in String")),
+        }
+    }
 }
